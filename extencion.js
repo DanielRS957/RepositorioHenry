3b65d0a1008ac6e61085f6542736959915f96738
DanielRS957 <122406740+DanielRS957@users.noreply.github.com>
2023-01-18 02:29:22 -0500

    Update extencion.js

--- extencion.js
+++ extencion.js
@@ -4,12 +4,12 @@ function sumatres(x) {
 }
 
 sumatres(4)
-
+//booleano
 console.log (1231232 < 22323443343)
-
+//variables
 var cajon1 = (300.000)
 var cajon2 = (5.000000)
 var cajon3 = (50.000)
-
+//booleano con variables
 console.log (cajon1>cajon2) 
-    
+console.log (cajon3=cajon1+cajon2)
