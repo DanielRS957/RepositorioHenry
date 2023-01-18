c2d6563363c710fa8c92fad6824990a001d7202c
DanielRS957 <drsp9501@icloud.com>
2023-01-18 02:23:38 -0500

    mi cuarto commit

---
+++ extencion.js
@@ -0,0 +1,15 @@
+function sumatres(x) {
+    console.log(x+3)
+    
+}
+
+sumatres(4)
+
+console.log (1231232 < 22323443343)
+
+var cajon1 = (300.000)
+var cajon2 = (5.000000)
+var cajon3 = (50.000)
+
+console.log (cajon1>cajon2) 
+    
