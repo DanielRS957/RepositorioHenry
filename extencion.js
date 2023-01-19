d14b25f29a13cd3163238c0749d199f125082e55
DanielRS957 <drsp9501@icloud.com>
2023-01-19 17:47:05 -0500

    ultimo repaso de commit

--- extencion.js
+++ extencion.js
@@ -7,9 +7,19 @@ sumatres(4)
 //booleano
 console.log (1231232 < 22323443343)
 //variables
-var cajon1 = (300.000)
-var cajon2 = (5.000000)
-var cajon3 = (50.000)
+var cajon1 = (299999);
+var cajon2 = (4999999);
+var cajon3 = (59999);
+//var cajon4 = (cajon1 = cajon2 = cajon3);
+
 //booleano con variables
 console.log (cajon1>cajon2) 
-console.log (cajon3=cajon1+cajon2)
+console.log (cajon2)
+console.log (cajon3)
+
+var nombre = ("daniel");
+var saludo = ("hola ");
+var edad = (27)
+
+console.log (saludo + nombre )
+console.log (edad)
